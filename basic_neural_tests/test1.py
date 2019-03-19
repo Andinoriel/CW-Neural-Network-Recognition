85849805c42e8094771a9ab21306d523b8d17099
Nicholas Simon <36269798+Andinoriel@users.noreply.github.com>
2019-03-19 17:38:31 +0200

    Test #1:  Recognition of numbers, 0.1
    Simple implementation of needed libraries to do work

---
+++ basic_neural_tests/test1.py
@@ -0,0 +1,29 @@
+import sys
+
+#   Recognition of handwritten numbers - TEST №1
+#   see more: http://yann.lecun.com/exdb/mnist/
+#   
+#   using libraries:
+#       numpy
+#       TensorFlow
+#       Keras
+
+
+import numpy # different math; arrays; matrix; very productive
+from keras.models import Sequential # linear stack of layers
+from keras.layers import Dense # neuro: activation func etc; FEEDFORWARD NEURALNETWWORK
+from keras.utils import np_utils # different utils
+
+# stochastic optimization setting: current value for the data repeatability
+numpy.random.seed(23)
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+else:
+    pass
